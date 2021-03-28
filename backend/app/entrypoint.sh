48f8d46cccec154e424bbdd1f282c366ba645ebe
madwayz <djurball@list.ru>
2021-03-29 03:03:15 +0700

    * Пофиксил архитектуру.

--- backend/app/entrypoint.sh
+++ backend/app/entrypoint.sh
@@ -9,7 +9,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 
-chmod -R 777 app/static
+chmod -R 777 static
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
@@ -20,4 +20,4 @@ then
 		--skip-checks
 fi
 
-exec gunicorn app.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
